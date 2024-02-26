534bf0fd277766603467866b884609a0a9238742
Ibrahim Althomali <ibrahimalthomali@gmail.com>
2024-02-26 19:49:21 +0300

    version number updated

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 var englishLocale = "en-SA";
 var arabicLocale = "ar-SA";
 var version = "برمجيات المسجد نسخة ";
-version += "1.2";
+version += "1.2.1";
 var logging = false;
 var testDate = undefined//new Date(2024, 1, 26, 18, 20, 50);//Y,M(starts at 0),D
 //-----End of Settings-------
